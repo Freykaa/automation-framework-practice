dc07586590133dd3a0fee4bbd4d78a3ead991b22
Inna Zaichenko <inna_zaichenko@epam.com>
2024-01-19 03:24:18 +0200

    moved project to src folder

---
+++ src/tests/specs/test_add_card.spec.js
@@ -0,0 +1,68 @@
+import chai from 'chai';
+
+const { expect } = chai;
+
+const login = async () => {
+    const loginBtn = await $('//a[@data-testid="login"]')
+    const loginUsername = await $('#username');
+    const submitBtn = await $('#login-submit');
+    const loginPassword = await $('#password');
+
+    await browser.url("https://trello.com/");
+    await loginBtn.click();
+    await loginUsername.setValue('motay64399@anawalls.com');
+    await submitBtn.click();
+    await loginPassword.waitForDisplayed({ timeout: 10000 });
+    await loginPassword.setValue('&mDXtUkEiiF$97&');
+    await submitBtn.click();
+    browser.waitUntil(
+        () => browser.execute(() => document.readyState === 'complete'),
+        {
+          timeout: 60 * 1000, // 60 seconds
+          timeoutMsg: 'Message on failure'
+        }
+)};
+
+const openBoardTab = async () => {
+    const boardTabBtn = await $('a[data-testid="home-team-boards-tab"]');
+    const boardSection = await $('ul.boards-page-board-section-list');
+    await boardTabBtn.click();
+    await boardSection.waitForDisplayed({timeout: 10000});
+};
+
+
+const chooseBoard = async () => {
+    const boardTile = $('a.board-tile');
+    await boardTile.click();
+
+};
+
+const addCard = async (cardValue) => {
+    const addCardBtn = await $('button[data-testid="list-add-card-button"]');
+    const addCardTitle = await $('textarea[data-testid="list-card-composer-textarea"]');
+    const saveCardBtn = await $('button[data-testid="list-card-composer-add-card-button"]');
+
+    await addCardBtn.waitForDisplayed({timeout: 10000});
+    await addCardBtn.click();
+    await addCardTitle.waitForDisplayed({timeout: 10000});
+    await addCardTitle.setValue(cardValue);
+    await saveCardBtn.click();
+    await saveCardBtn.click();
+};
+
+describe("Add card", () => {
+     it("Add default card to the board", async () => {
+        const cardName = await $('a[data-testid="card-name"]');
+        const cardValue = 'newcard';
+
+        await login();
+        await openBoardTab();
+        await chooseBoard();
+        await addCard(cardValue);
+
+        const getCardName = await cardName.getText();
+        expect(getCardName).to.equal(cardValue);
+    });
+});
+
+
---
+++ src/tests/specs/test_edit_workspace.spec.js
@@ -0,0 +1,55 @@
+import chai from 'chai';
+
+const { expect } = chai;
+
+const login = async () => {
+    const loginBtn = await $('//a[@data-testid="login"]')
+    const loginUsername = await $('#username');
+    const submitBtn = await $('#login-submit');
+    const loginPassword = await $('#password');
+
+    await browser.url("https://trello.com/");
+    await loginBtn.click();
+    await loginUsername.setValue('motay64399@anawalls.com');
+    await submitBtn.click();
+    await loginPassword.waitForDisplayed({ timeout: 3000 });
+    await loginPassword.setValue('&mDXtUkEiiF$97&');
+    await submitBtn.click();
+    browser.waitUntil(
+        () => browser.execute(() => document.readyState === 'complete'),
+        {
+          timeout: 60 * 1000, // 60 seconds
+          timeoutMsg: 'Message on failure'
+        }
+)};
+
+const editWorkspaceName = async (newWorkspaceName) => {
+    const editIcon = await $('span[data-testid="EditIcon"]');
+    const nameInput = await $('input#displayName');
+    const saveButton = await $('form>button[type=submit]');
+    const workspaceName = await $('span[data-testid="home-team-tab-name"]');
+
+    await editIcon.click();
+    await nameInput.setValue(newWorkspaceName);
+    await saveButton.click();
+    await workspaceName.waitForDisplayed({timeout: 10000});
+};
+
+const openBoardTab = async () => {
+    const boardTabBtn = await $('a[data-testid="home-team-boards-tab"]');
+
+    await boardTabBtn.click();
+}
+
+describe("Edit workspace", () => {
+    it("Edit workspace name", async () => {
+        const newWorkspaceName = 'testing space';
+
+        await login();
+        await openBoardTab();
+        await editWorkspaceName(newWorkspaceName);
+
+        const getHomeWorkspaceName = await $('span[data-testid="home-team-tab-name"]').getText();
+        expect(getHomeWorkspaceName).to.equal(newWorkspaceName);
+    });
+});
