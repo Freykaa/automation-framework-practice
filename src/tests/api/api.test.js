e2055c6b97312a03322dfe26b66e3950183123e1
Inna Zaichenko <Inna_Zaichenko@epam.com>
2024-04-16 20:12:12 +0300

    updated api test

--- src/tests/api/api.test.js
+++ src/tests/api/api.test.js
@@ -1,30 +1,41 @@
-import request  from 'supertest';
-import 'dotenv/config';
+import request from 'supertest';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const apiKey = process.env.TRELLO_API_KEY;
+const apiToken = process.env.TRELLO_API_TOKEN;
 
 describe('Trello API', () => {
+    let boardId;
+
     it('should retrieve all boards associated with the member', (done) => {
         request('https://api.trello.com')
             .get(`/1/members/me/boards?key=${apiKey}&token=${apiToken}`)
-            .expect(200, done)
+            .expect(200, done);
     });
 
     it('should create a board', (done) => {
         request('https://api.trello.com')
-            .post(`/1/boards/?name=pupupu&key=${apiKey}&token=${apiToken}`) // get board id here
-            .expect(200, done)
+            .post(`/1/boards/?name=pupupu&key=${apiKey}&token=${apiToken}`)
+            .expect(200)
+            .then(response => {
+                boardId = response.body.id;
+                done();
+            })
+            .catch(error => done(error));
     });
 
     it('update a board name', (done) => {
         request('https://api.trello.com')
-            .put(`/1/boards/661e8c414527a7741a21f5f1?key=${apiKey}&token=${apiToken}`)
+            .put(`/1/boards/${boardId}?key=${apiKey}&token=${apiToken}`)
             .send({ name: 'update' })
-            .expect(200, done)
+            .expect(200, done);
     });
 
     it('should delete a board', (done) => {
         request('https://api.trello.com')
-            .del(`/1/boards/661e917b4cd08c454ea372cf?key=${apiKey}&token=${apiToken}`) // take board id from previous test
-            .expect(200, done)
+            .del(`/1/boards/${boardId}?key=${apiKey}&token=${apiToken}`)
+            .expect(200, done);
     });
-
 });
